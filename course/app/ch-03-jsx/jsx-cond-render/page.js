34d9be11921c245db23f90321b48b25db8bd12fe
Eddy Chang <eddy@joomla.com.tw>
2025-06-12 11:17:02 +0800

    JSX條件式渲染

--- course/app/ch-03-jsx/jsx-cond-render/page.js
+++ course/app/ch-03-jsx/jsx-cond-render/page.js
@@ -24,6 +24,18 @@ export default function JsxCondRenderPage() {
       >
         -1
       </button>
+      {/* 三元運算子 */}
+      {total !== 0 ? <p>1.目前total的值是{total}</p> : ''}
+      {total > 0 ? <p>2.目前total的值是{total}</p> : ''}
+      {/* 內聯式if (inline if) */}
+      {/* 因為`&&`會使用falsy進行運算，0與NaN都會呈現，造成不精確的結果 */}
+      {total && <p>3.目前total的值是{total}</p>}
+      {/* 將用於條件的運算式(值)強制轉為布林值 */}
+      {Boolean(total) && <p>4.目前total的值是{total}</p>}
+      {!!total && <p>4.目前total的值是{total}</p>}
+      {/* 用比較運算子，寫清楚條件 */}
+      {total !== 0 && <p>5.目前total的值是{total}</p>}
+      {total > 0 && <p>6.目前total的值是{total}</p>}
     </>
   )
 }
