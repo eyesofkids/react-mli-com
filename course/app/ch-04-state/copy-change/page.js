a9528fcd6ba504c37d0806ae03b7fff63f11c80f
Eddy Chang <eddy@joomla.com.tw>
2025-06-12 14:32:48 +0800

    拷貝與狀態更動-狀態變動共通三步驟

--- course/app/ch-04-state/copy-change/page.js
+++ course/app/ch-04-state/copy-change/page.js
@@ -18,9 +18,42 @@ export default function CopyChangePage() {
       <h1>拷貝與狀態更動</h1>
       <hr />
       <p>{JSON.stringify(user)}</p>
-      <button onClick={() => {}}>姓名改為 李四</button>
-      <button onClick={() => {}}>電話改為 0912345678</button>
-      <button onClick={() => {}}>城市改為 新北市</button>
+      <button
+        onClick={() => {
+          // 1. 從目前的狀態拷貝出一個副本(必要時深拷貝)
+          const nextUser = JSON.parse(JSON.stringify(user))
+          // 2. 在拷貝出來的新副本上更動
+          nextUser.name = '李四'
+          // 3. 呼叫setState設定到狀態
+          setUser(nextUser)
+        }}
+      >
+        姓名改為 李四
+      </button>
+      <button
+        onClick={() => {
+          // 1. 從目前的狀態拷貝出一個副本(必要時深拷貝)
+          const nextUser = JSON.parse(JSON.stringify(user))
+          // 2. 在拷貝出來的新副本上更動
+          nextUser.profile.phone = '0912345678'
+          // 3. 呼叫setState設定到狀態
+          setUser(nextUser)
+        }}
+      >
+        電話改為 0912345678
+      </button>
+      <button
+        onClick={() => {
+          // 1. 從目前的狀態拷貝出一個副本(必要時深拷貝)
+          const nextUser = JSON.parse(JSON.stringify(user))
+          // 2. 在拷貝出來的新副本上更動
+          nextUser.profile.address.city = '新北市'
+          // 3. 呼叫setState設定到狀態
+          setUser(nextUser)
+        }}
+      >
+        城市改為 新北市
+      </button>
     </>
   )
 }
