7a05042ca2e9d65b34cfdb31d10b9a8b46b6e2cb
Eddy Chang <eddy@joomla.com.tw>
2025-06-12 15:31:53 +0800

    物件陣列狀態練習-第3

---
+++ course/app/ch-04-state/object-array/page.js
@@ -0,0 +1,131 @@
+'use client'
+
+import { useState } from 'react'
+
+const sample = [
+  {
+    id: 1,
+    text: 'a',
+  },
+  {
+    id: 2,
+    text: 'b',
+  },
+  {
+    id: 3,
+    text: 'c',
+  },
+  {
+    id: 4,
+    text: 'aa',
+  },
+]
+
+export default function ObjectArrayPage() {
+  // 呈現(渲染)時會與使用者互動時進行改動，必需是state
+  const [data, setData] = useState(sample)
+
+  return (
+    <>
+      <h1>物件陣列(object array)狀態的各種操作</h1>
+      <hr />
+      <h2>資料表格</h2>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Text</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((v, i) => {
+            return (
+              <tr key={v.id}>
+                <td>{v.id}</td>
+                <td>{v.text}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <hr />
+      <h2>操作</h2>
+      <p>
+        <strong>
+          注意: 請在任一操作前先重新整理網頁 ，或是對重覆id值進行加入時的限制。
+        </strong>
+        有些操作是key值會對應id的關係，會產生key值重覆問題，造成不預期的結果。實務上務必要考慮key不可以重覆問題。
+      </p>
+
+      <button
+        onClick={() => {
+          // 先寫出要新增的物件值
+          const newObj = { id: 99, text: 'xxx' }
+
+          // 1. 從目前的狀態拷貝出一個新的變數值(陣列/物件)
+          // 2. 在新的變數值(陣列/物件)上作處理
+          // 3. 設定回原本的狀態中
+
+          //1 //2
+          const nextData = [newObj, ...data]
+
+          //3
+          setData(nextData)
+        }}
+      >
+        1. 列表最前面，新增一個物件值id為99與文字為xxx的物件
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          const newObj = { id: 88, text: 'yyy' }
+
+          //1 //2
+          const nextData = [...data, newObj]
+
+          //3
+          setData(nextData)
+        }}
+      >
+        2. 列表最後面，新增一個物件值id為88與文字為yyy的物件
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          // 產生不重覆id的方式
+          // 1. 使用uuid(或其它能產生亂數雜湊id的函式庫)
+          // const newId = self.crypto.randomUUID()
+          // 2. 使用時間日期物件(轉字串或整數值)
+          // 以下可以使用 `Date.now()` 或 `+new Date()`
+          const newId = Number(new Date())
+          // 先寫出要新增的物件值
+          const newObj = { id: newId, text: 'xxx' }
+
+          //1 //2
+          const nextData = [newObj, ...data]
+
+          //3
+          setData(nextData)
+        }}
+      >
+        3. 列表最前面，新增一個文字為xxx的物件(id不能與其它資料重覆)
+      </button>
+      <br />
+      <button onClick={() => {}}>
+        4. 列表最後面，新增一個文字為yyy的物件(id不能與其它資料重覆)
+      </button>
+      <br />
+      <button onClick={() => {}}>
+        5. 尋找(過濾)只呈現所有文字中有包含a英文字母的資料
+      </button>
+      <br />
+      <button onClick={() => {}}>6. 刪除文字為b的物件資料</button>
+      <br />
+      <button onClick={() => {}}>7. 刪除id為4的物件資料</button>
+      <br />
+      <button onClick={() => {}}>8. 取代id為3的文字為cccc</button>
+      <br />
+      <button onClick={() => {}}>9. 清空表格</button>
+    </>
+  )
+}
