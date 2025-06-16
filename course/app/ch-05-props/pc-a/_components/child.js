d6e81587f39368b7189da627c7f2617f50335e4c
Eddy Chang <eddy@joomla.com.tw>
2025-06-16 15:07:31 +0800

    Props屬性範例-a_props預設值解說

--- course/app/ch-05-props/pc-a/_components/child.js
+++ course/app/ch-05-props/pc-a/_components/child.js
@@ -1,14 +1,27 @@
 'use client'
 // 子女元件可以透過函式的傳入參數值，得到父母元件傳來的屬性值
 // props必定為一個物件
-export default function Child(props) {
-  console.log(props)
+// 傳入參數物件解構語法+預設值語法
 
+export default function Child({
+  title = '',
+  price = 0,
+  isConnected = true,
+  aa = [],
+  oa = {},
+  sum = () => {},
+  jsx = <></>,
+}) {
   return (
     <>
       <h3>Child(子女元件)</h3>
-      <p>{props.title}</p>
-      <p>{props.price}</p>
+      <p>title={title}</p>
+      <p>price={price}</p>
+      <p>isConnected={JSON.stringify(isConnected)}</p>
+      <p>aa={JSON.stringify(aa)}</p>
+      <p>oa={JSON.stringify(oa)}</p>
+      <p>sum(1,2)={sum(1, 2)}</p>
+      {jsx}
     </>
   )
 }
