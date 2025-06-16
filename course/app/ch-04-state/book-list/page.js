9bbbc6695b1bad49763bc07f4c9a5dc0b9ee7fb4
Eddy Chang <eddy@joomla.com.tw>
2025-06-16 10:51:28 +0800

    練習4-books資料擴充布林值屬性作法

--- course/app/ch-04-state/book-list/page.js
+++ course/app/ch-04-state/book-list/page.js
@@ -12,6 +12,27 @@ import bookmarkIconFill from './_icons/bookmark-fill.svg'
 import bookmarkIcon from './_icons/bookmark.svg'
 
 export default function BookListPage() {
+  // 擴充原本的資料多一個屬性來代表是否有加入收藏(布林值，預設為false)
+  const initState = data.map((v) => {
+    return { ...v, bookmark: false }
+  })
+
+  // 宣告狀態
+  const [books, setBooks] = useState(initState)
+
+  // 處理點按事件
+  const handleToggleBookmark = (bookIsbn) => {
+    const nextBooks = books.map((book) => {
+      // 如果找到isbn為booksIsbn的成員，則作bookmark屬性的反相
+      if (book.isbn === bookIsbn) return { ...book, bookmark: !book.bookmark }
+      // 其它物件直接返回
+      else return book
+    })
+
+    // 3
+    setBooks(nextBooks)
+  }
+
   return (
     <>
       <h1>書籍清單</h1>
@@ -19,20 +40,30 @@ export default function BookListPage() {
         <thead>
           <tr>
             <th>ISBN</th>
-            <th>title</th>
-            <th>author</th>
+            <th>標題</th>
+            <th>作者</th>
             <th>加入收藏</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>XXX</td>
-            <td>XXX</td>
-            <td>XXX</td>
-            <td>
-              <Image src={bookmarkIcon} alt="" />
-            </td>
-          </tr>
+          {books.map((book) => {
+            return (
+              <tr key={book.isbn}>
+                <td>{book.isbn}</td>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>
+                  <Image
+                    onClick={() => {
+                      handleToggleBookmark(book.isbn)
+                    }}
+                    src={book.bookmark ? bookmarkIconFill : bookmarkIcon}
+                    alt=""
+                  />
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </>
