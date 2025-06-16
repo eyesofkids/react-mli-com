e5dec473f854228a3fefd6e6efdd425d6bd19fdf
Eddy Chang <eddy@joomla.com.tw>
2025-06-16 15:45:26 +0800

    +註解

--- course/app/ch-05-props/pc-b/_components/child-b.js
+++ course/app/ch-05-props/pc-b/_components/child-b.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react'
 export default function ChildB({ setDataFromChild = () => {} }) {
   const [cData, setCData] = useState('child-b data')
 
-  // 在一開始呈現時呼叫回送資料的函式
+  // 解決方案1: 在一開始呈現時呼叫回送資料的函式
   useEffect(() => {
     setDataFromChild(cData)
   }, [])
@@ -15,6 +15,7 @@ export default function ChildB({ setDataFromChild = () => {} }) {
       <h3>Child(子女元件)-b</h3>
       <button
         onClick={() => {
+          // 解決方案2: 事件觸發時的處理函式中呼叫回送資料的函式
           setDataFromChild(cData)
         }}
       >
