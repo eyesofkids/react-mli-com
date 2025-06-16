e8ed91986718ebaf386aeb32d333fa0d5650494c
Eddy Chang <eddy@joomla.com.tw>
2025-06-16 11:48:45 +0800

    練習5-處理點按

--- course/app/ch-04-state/star/page.js
+++ course/app/ch-04-state/star/page.js
@@ -1,20 +1,39 @@
 'use client'
 
-import React from 'react'
+import { useState } from 'react'
 // 導入.module.css檔案
 import styles from './_styles/star.module.css'
 
 export default function StarPage() {
+  // 目前的評分記錄，一開始為0代表未點按
+  const [rating, setRating] = useState(0)
+
   return (
     <>
       <h1>星星評分範例</h1>
+      <hr />
       <div>
-        <span className={styles.goodStyle}>&#9733;</span>
-        <span>&#9733;</span>
-        <span>&#9733;</span>
-        <span>&#9733;</span>
-        <span>&#9733;</span>
+        {Array(5)
+          .fill(1)
+          .map((v, i) => {
+            const score = i + 1
+
+            return (
+              <button
+                key={i}
+                onClick={() => {
+                  setRating(score)
+                }}
+                className={styles.starBtn}
+              >
+                <span className={score <= rating ? styles.on : styles.off}>
+                  &#9733;
+                </span>
+              </button>
+            )
+          })}
       </div>
+      <p>目前點按: {rating} 分</p>
     </>
   )
 }
