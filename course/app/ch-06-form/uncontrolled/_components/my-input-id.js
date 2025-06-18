4994033d95ec10ae5327c728561ad8dfd41ad337
Eddy Chang <eddy@joomla.com.tw>
2025-06-18 16:05:32 +0800

    表單_不可控表單元件_架構

---
+++ course/app/ch-06-form/uncontrolled/_components/my-input-id.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function MyInputId() {
+  return (
+    <>
+      <input type="text" />
+    </>
+  )
+}
---
+++ course/app/ch-06-form/uncontrolled/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import MyInputId from './_components/my-input-id'
+
+export default function UncontrolledPage() {
+  return (
+    <>
+      <h1>不可控表單元件(uncontrolled)</h1>
+      <hr />
+      <MyInputId />
+    </>
+  )
+}
