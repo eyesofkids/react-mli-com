052694100de8356be1cffd235875a00c02dbbe53
Eddy Chang <eddy@joomla.com.tw>
2025-06-18 16:19:32 +0800

    +註解

--- course/app/ch-06-form/uncontrolled/_components/my-input-id.js
+++ course/app/ch-06-form/uncontrolled/_components/my-input-id.js
@@ -1,11 +1,15 @@
 'use client'
 
 export default function MyInputId() {
+  // 在這裡執行是得不到"虛擬DOM"元素的物件實體的(執行周期的關係)
+  // console.log(document.querySelector('#my-input'))
+
   return (
     <>
       <input type="text" id="my-input" />
       <button
         onClick={() => {
+          // 要在事件處理函式，或是useEffect中，才能得到"虛擬DOM"的物件實體
           console.log(document.querySelector('#my-input'))
 
           alert(document.querySelector('#my-input').value)
