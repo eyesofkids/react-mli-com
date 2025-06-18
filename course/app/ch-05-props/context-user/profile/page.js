6153fa6f4f2993f238b6ed17947107d7e61b3f77
Eddy Chang <eddy@joomla.com.tw>
2025-06-18 10:17:38 +0800

    context範例-第2步_頁面中使用useContext

--- course/app/ch-05-props/context-user/profile/page.js
+++ course/app/ch-05-props/context-user/profile/page.js
@@ -1,16 +1,20 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import { useContext } from 'react'
+import { UserContext } from '@/context/user'
 // 使用Link元件取代a標記(連結)
 // 可以保持目前的應用的狀態值(尤其是針對context中的狀態)
 import Link from 'next/link'
 
 export default function ProfilePage() {
+  const { name, email } = useContext(UserContext)
   return (
     <>
       <h1>個人資料頁</h1>
       <Link href="/ch-05-props/context-user/login">登入頁</Link>
       <hr />
+      <p>name={name}</p>
+      <p>email={email}</p>
     </>
   )
 }
