b9d2f13aef25915596b218184704da3681578038
Eddy Chang <eddy@joomla.com.tw>
2025-06-18 16:17:49 +0800

    表單_不可控表單元件_使用id

--- course/app/ch-06-form/uncontrolled/_components/my-input-id.js
+++ course/app/ch-06-form/uncontrolled/_components/my-input-id.js
@@ -3,7 +3,30 @@
 export default function MyInputId() {
   return (
     <>
-      <input type="text" />
+      <input type="text" id="my-input" />
+      <button
+        onClick={() => {
+          console.log(document.querySelector('#my-input'))
+
+          alert(document.querySelector('#my-input').value)
+        }}
+      >
+        獲得值
+      </button>
+      <button
+        onClick={() => {
+          document.querySelector('#my-input').value = 'abcde'
+        }}
+      >
+        設定值
+      </button>
+      <button
+        onClick={() => {
+          document.querySelector('#my-input').focus()
+        }}
+      >
+        聚焦(focus)
+      </button>
     </>
   )
 }
