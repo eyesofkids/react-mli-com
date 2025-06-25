caf8b1a72f8c932378b0c79e7f5eef3ffda83af3
Eddy Chang <eddy@joomla.com.tw>
2025-06-25 14:13:15 +0800

    練習8-列表&詳細頁_載入指示動畫

--- course/app/ch-07-effect/insurance-sp/page.js
+++ course/app/ch-07-effect/insurance-sp/page.js
@@ -3,11 +3,14 @@
 import { useState, useEffect } from 'react'
 // 連結用
 import Link from 'next/link'
+// 載入指示動畫
+import CssLoader from './_components/css-loader'
 
-// https://my-json-server.typicode.com/eyesofkids/json-fake-data/insurance
 export default function InsurancePage() {
   // 接收從伺服器來的資料(物件陣列)
   const [insurances, setInsurances] = useState([])
+  // 一個指示是否正在載入資料的狀態
+  const [loading, setLoading] = useState(true)
 
   // 向伺服器獲取資料的函式
   const getData = async () => {
@@ -18,6 +21,10 @@ export default function InsurancePage() {
     console.log(resData)
     // 設定到狀態，觸發重新渲染
     setInsurances(resData)
+    // 關閉載入指示
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
   }
 
   // 樣式2
@@ -26,6 +33,16 @@ export default function InsurancePage() {
     // 呼叫async函連接伺服器得到資料
     getData()
   }, [])
+
+  if (loading) {
+    return (
+      <>
+        <h1>保險單列表頁(SearchParams)</h1>
+        <hr />
+        <CssLoader />
+      </>
+    )
+  }
 
   return (
     <>
