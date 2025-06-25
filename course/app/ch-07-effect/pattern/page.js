6cc8ea3a34b89888b3a35c69a1a90ec9d7f498c3
Eddy Chang <eddy@joomla.com.tw>
2025-06-25 10:43:12 +0800

    useEffect的4種樣式_第4樣式

--- course/app/ch-07-effect/pattern/page.js
+++ course/app/ch-07-effect/pattern/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function PatternPage() {
   const [total, setTotal] = useState(0)
@@ -21,15 +22,25 @@ export default function PatternPage() {
   }, [])
 
   // 樣式3: 有第二個傳入參數(裡面有相依變數)
-  // 意義: 第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。
+  // 意義: 第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。類似於componentDidMount+componentDidUpdate生命周期方法。
+  // 用途: 狀態異步解決方案。狀態連鎖更動。
   useEffect(() => {
-    console.log('第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。')
+    //console.log('第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。')
   }, [total])
   // ^^^^^^^ 這裡放入total狀態，是要監聽total的更動事件(change)
+
+  // 樣式4: 第一個傳入參數(函式)的回傳值，本身也是一個函式
+  // 意義: 元件被移出之前執行一次，稱為cleanup(清理函式)
+  useEffect(() => {
+    return () => {
+      console.log('元件被移出之前執行一次')
+    }
+  }, [])
 
   return (
     <>
       <h1>useEffect的4種樣式</h1>
+      <Link href="/">連至 首頁</Link>
       <hr />
       <button
         onClick={() => {
