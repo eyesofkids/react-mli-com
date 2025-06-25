d962bec3a76eccde006170e7de6836efdf7eba6a
Eddy Chang <eddy@joomla.com.tw>
2025-06-25 11:48:23 +0800

    練習8-列表&詳細頁_架構

---
+++ course/app/ch-07-effect/insurance/[id]/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+// 動態路由參數
+export default function IdPage() {
+  return (
+    <>
+      <h1>保險單詳細頁</h1>
+      <hr />
+    </>
+  )
+}
---
+++ course/app/ch-07-effect/insurance/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+export default function InsurancePage() {
+  return (
+    <>
+      <h1>保險單列表頁</h1>
+      <hr />
+    </>
+  )
+}
