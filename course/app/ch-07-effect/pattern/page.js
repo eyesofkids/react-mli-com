a976db3d2fda4d18db32e5ae3f2a15c9a7118b55
Eddy Chang <eddy@joomla.com.tw>
2025-06-25 11:21:37 +0800

    引用相等性原則解說

--- course/app/ch-07-effect/pattern/page.js
+++ course/app/ch-07-effect/pattern/page.js
@@ -5,14 +5,20 @@ import Link from 'next/link'
 
 export default function PatternPage() {
   const [total, setTotal] = useState(0)
+  // total 0 === 1
   const [total2, setTotal2] = useState(0)
+
+  const [count, setCount] = useState({ value: 1 })
+  // count {value:1} === {value:1}
 
   // 樣式1: 沒有第二個傳入參數
   // 意義: 每次渲染之後都會執行
   // 用途: 特殊自訂勾子，作日誌、除錯用
   useEffect(() => {
-    // console.log('每次渲染之後都會執行')
+    console.log('每次渲染之後都會執行')
   })
+
+  console.log('每次渲染都會執行')
 
   // 樣式2: 有第二個傳入參數(但保持空陣列)
   // 意義: 第一次渲染後執行一次，之後不會再執行。類似於componentDidMount生命周期方法。
@@ -25,9 +31,10 @@ export default function PatternPage() {
   // 意義: 第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。類似於componentDidMount+componentDidUpdate生命周期方法。
   // 用途: 狀態異步解決方案。狀態連鎖更動。
   useEffect(() => {
-    //console.log('第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。')
+    console.log('第一次渲染後執行一次，之後在相依變數有更動之後，再執行一次。')
   }, [total])
   // ^^^^^^^ 這裡放入total狀態，是要監聽total的更動事件(change)
+  // total `1 === 2`
 
   // 樣式4: 第一個傳入參數(函式)的回傳值，本身也是一個函式
   // 意義: 元件被移出之前執行一次，稱為cleanup(清理函式)
@@ -54,7 +61,16 @@ export default function PatternPage() {
           setTotal2(total2 + 1)
         }}
       >
-        total2={total2}
+        total2
+      </button>
+      <h2>{count.value}</h2>
+      <button
+        onClick={() => {
+          // {value:1} === {value:1}
+          setCount({ value: 1 })
+        }}
+      >
+        count.value=1
       </button>
     </>
   )
